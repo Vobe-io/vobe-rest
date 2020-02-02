6bc9523be755d74f718bef9b429cae050159a2a1
Comesa <mccomesa@gmail.com>
2020-02-02 16:27:08 +0100

    Improved /profile request

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,11 @@
 let express = require('express');
 let router = express.Router();
+let User = require(__models + '/user.js');
 
 /* GET home page. */
-router.get('/:username', function (req, res, next) {
-    res.render('profile', {username: req.params.username});
+router.get('/:username', async function (req, res, next) {
+    let user = await User.findOne({username: req.params.username});
+    res.render('profile', {user: user});
 });
 
 module.exports = {index: 1, router: router};
