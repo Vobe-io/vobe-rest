5ba39accf9fbd74e8b72b2c5509126f8c773d0e1
Timo Aeckerle <timoaeckerle@gmx.de>
2020-02-02 12:00:58 +0100

    Update post route with new aggregation method

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,37 @@
 let express = require('express');
 let router = express.Router();
+let Post = require(__models + '/post.js');
+var ObjectId = require('mongodb').ObjectID;
 
 /* GET home page. */
-router.get('/v/:postID', function (req, res, next) {
-    res.render('post', {postID: req.params.postID});
+router.get('/v/:postID', async function (req, res, next) {
+    let posts = await Post.aggregate([{
+        $match: {
+            _id: ObjectId(req.params.postID)
+        }
+    }, {
+        $lookup: {
+            from: "users",
+            localField: "owner",
+            foreignField: "_id",
+            as: "ownerData"
+        }
+    }, {
+        $unwind: {
+            path: '$ownerData'
+        }
+    }, {
+        $lookup: {
+            from: 'posts',
+            localField: '_id',
+            foreignField: 'parent',
+            as: 'children'
+        }
+    }])
+        .sort({date: -1})
+        .limit(20);
+
+    res.render('post', {posts: posts});
 });
 
 module.exports = {index: 0, router: router};
