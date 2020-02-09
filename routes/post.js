71db4edd12a70b0df6f8fd45657e6a8cdae1007b
Comesa <mccomesa@gmail.com>
2020-02-09 19:48:58 +0100

    Fixed issue #19

--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,16 @@
 let express = require('express');
 let router = express.Router();
 let Post = require(__models + '/post.js');
+let bson = require('bson');
 
 /* GET home page. */
-router.get('/v/:postID', async function (req, res, next) {
+router.get('/v/:postId', async function (req, res, next) {
 
-    let posts = await Post.getRichPost(1);
+    let posts = await Post.getRichPost([{
+        $match: {_id: bson.ObjectId.createFromHexString(req.params.postId)}
+    },{
+        $limit: 1
+    }]);
 
     res.render('post', {
         posts: posts,
