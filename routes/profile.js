c244fcd508b7e63037b681c36ec85d2c4381d5cc
Comesa <mccomesa@gmail.com>
2020-02-09 19:19:43 +0100

    resolved issue#5

--- routes/profile.js
+++ routes/profile.js
@@ -4,7 +4,11 @@ let User = require(__models + '/user.js');
 
 /* GET home page. */
 router.get('/:username', async function (req, res, next) {
-    res.render('profile', {username: req.params.username});
+    res.render('profile', {
+        params: req.params,
+        user: req.user,
+        profile: await User.findOne({username: req.params.username})
+    });
 });
 
 module.exports = {index: 1, router: router};
