5f87717bcd5a80c87ba5dd0283cb44c118b288ce
Comesa <mccomesa@gmail.com>
2020-02-09 14:40:59 +0100

    fixed unused require

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,6 @@
 let express = require('express');
 let router = express.Router();
 let Post = require(__models + '/post.js');
-let ObjectId = require('mongodb').ObjectID;
 
 /* GET home page. */
 router.get('/v/:postID', async function (req, res, next) {
