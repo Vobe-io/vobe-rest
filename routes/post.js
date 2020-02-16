8e95fcf6d859fecc77407727a4f1b951e3edb096
Timo Aeckerle <timoaeckerle@gmx.de>
2020-02-16 05:27:44 +0100

    Fixing issue#47 - Invalid Hex-Id on post route
    + Fixing broken error messages

--- routes/post.js
+++ routes/post.js
@@ -6,17 +6,16 @@ let response = require(__bin + '/lib/Response');
 
 router.post('/v', async function (req, res, next) {
 
-    let isHex = /[0-9A-Fa-f]{6}/g;
-    let posts = '';
-    if (isHex.test(req.body.postId))
-        posts = await Post.getRichPost([{
+    try {
+        let posts = await Post.getRichPost([{
             $match: {_id: bson.ObjectId.createFromHexString(req.body.postId)}
         }, {
             $limit: 1
         }]);
-
-    else return res.status(400).send(response('', 'Invalid post ID'));
-    res.status(200).send(response(posts, ''));
+        res.status(200).send(response(posts, ''));
+    } catch (e) {
+        res.status(400).send(response('', 'Invalid post ID'));
+    }
 });
 
 module.exports = {
